731de59a892b7ff21ca231bf31087243bc0c19b1
Zhe Yang <yang.zhe@msn.com>
2018-06-19 12:49:51 +0800

    处理str、bytes类型转换

--- WXBizMsgCrypt.py
+++ WXBizMsgCrypt.py
@@ -141,7 +141,7 @@ class Prpcrypt(object):
         @return: 加密得到的字符串
         """      
         # 16位随机字符串添加到明文开头
-        text = self.get_random_str() + struct.pack("I",socket.htonl(len(text))) + text + corpid
+        text = self.get_random_str() + struct.pack("I",socket.htonl(len(text))).decode('utf-8') + text + corpid
         # 使用自定义的填充方式对明文进行补位填充
         pkcs7 = PKCS7Encoder()
         text = pkcs7.encode(text)
